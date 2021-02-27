ec31ca399e92c227c1e316a086f42b874ca25bdd
Neil Crowley <neilccrowley@gmail.com>
2021-02-27 11:28:54 +0000

    added arrow keys an pinball

--- code/code.js
+++ code/code.js
@@ -8,10 +8,19 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("slideshow");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    x[slideIndex-1].style.display = "block";
+    x[slideIndex - 1].style.display = "block";
 }
+
+document.onkeydown = (e) => {
+    if (e.key === "ArrowLeft") {
+        plusDivs(-1);
+    }
+    else if (e.key === "ArrowRight") {
+        plusDivs(1);
+    }
+} 
