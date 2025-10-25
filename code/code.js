04d62c6d979b7e32e254405f288f5d7200e9511f
Neil Crowley <neilccrowley@gmail.com>
2025-10-25 19:00:09 +0100

    Added captions

--- code/code.js
+++ code/code.js
@@ -1,9 +1,18 @@
 var slideIndex = 1;
 const slides = document.getElementsByClassName("slideshow");
+var slideTimer;
+
 showDivs(slideIndex);
+resetTimer();
+
+function resetTimer() {
+    clearInterval(slideTimer);
+    slideTimer = setInterval(() => plusDivs(1), 5000);
+}
 
 function plusDivs(n) {
     showDivs(slideIndex += n);
+    resetTimer();
 }
 
 function showDivs(n) {
@@ -18,10 +27,36 @@ function showDivs(n) {
 window.onkeydown = (gfg) => {
     if (gfg.keyCode === 37) {
         plusDivs(-1)
-    }
-    else if (gfg.keyCode === 39) {
+    } else if (gfg.keyCode === 39) {
         plusDivs(1);
     }
 }
 
-setInterval(() => plusDivs(1), 5000);
+const photoContainer = document.getElementById('photos');
+
+let touchstartX = 0;
+let touchendX = 0;
+const swipeThreshold = 50;
+
+function handleSwipe() {
+    const deltaX = touchendX - touchstartX;
+
+    if (Math.abs(deltaX) < swipeThreshold) {
+        return;
+    }
+
+    if (deltaX < 0) {
+        plusDivs(1);
+    } else {
+        plusDivs(-1);
+    }
+}
+
+photoContainer.addEventListener('touchstart', e => {
+    touchstartX = e.changedTouches[0].screenX;
+}, { passive: true });
+
+photoContainer.addEventListener('touchend', e => {
+    touchendX = e.changedTouches[0].screenX;
+    handleSwipe();
+}, { passive: true });
